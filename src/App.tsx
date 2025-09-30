508fb49fbc7c751444191f56638e6e0becc62ca5
김도현 <127471879+okiidokim@users.noreply.github.com>
2025-09-30 21:10:03 +0900

    [FEAT] next.js와 똑같은 상태로 만들기
    - UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen gap-16 p-16">
+      <header className="row-start-1 flex gap-[24px] flex-wrap items-center justify-center">
+        <span>헤더입니다.</span>
+      </header>
+      <main className="flex flex-col gap-[32px] row-start-2 items-start">
+        <h1>Next.js에서 폰트 최적화 시험하기</h1>
+        <div>
+          <h3>
+            FOIT<span>&nbsp;(Flash Of Invisible Text)</span>
+          </h3>
+          <div>실제 폰트가 로드되기 전까지 보이지 않음</div>
+        </div>
+        <div>
+          <h3>
+            FOUT<span>&nbsp;(Flash Of Unstyled Text)</span>
+          </h3>
+          <div>
+            실제 폰트가 로드되기 전까지 시스템 폰트/fallback 폰트를 보인 후 실제
+            폰트로 변경함
+          </div>
+        </div>
+      </main>
+      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+        <span>푸터입니다.</span>
+      </footer>
+    </div>
+  );
 }
 
-export default App
+export default App;
